01ecafe27cb8f525da40fe657b590d97192a0b72
suhan8984 <suhan8984@daum.net>
2024-10-01 14:52:36 +0900

    fix: typo

--- src/main/scala/04_Call_by.scala
+++ src/main/scala/04_Call_by.scala
@@ -2,7 +2,7 @@ def show(x: Int, y: =>Int): Int =
   println(y)
   x
 
-@main def reference(): Unit =
+@main def call_by(): Unit =
   def val1 = show(1, 5)
   val val2 = show(2, 3) // print 3
   println(val1) // print 5, 1
